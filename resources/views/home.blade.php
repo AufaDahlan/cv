18c7afd7766f8927dbf27ca17b8b087ea575bf53
User <User@DESKTOP-TGILV81.com>
2022-11-11 15:06:07 +0700

    first

---
+++ resources/views/home.blade.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume/CV Design</title>
+	<link rel="stylesheet" href="{{asset('stylescv.css')}}">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="{{asset('Foto.jpeg')}}" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">Aufa Dahlan</p>
+           <p class="regular">Web Designer | Desain Grafis</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Lamlhom Kec. Lhoknga <br /> Indonesia , Banda Aceh
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               +62895637331419
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               aufadahlan.sc <br /> @gmail.com
+             </div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skill's</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               HTML
+             </div>
+             <div class="skill_progress">
+               <span style="width: 70%;"></span>
+             </div>
+             <div class="skill_per">70%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               CSS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 50%;"></span>
+             </div>
+             <div class="skill_per">50%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Ai
+             </div>
+             <div class="skill_progress">
+               <span style="width: 90%;"></span>
+             </div>
+             <div class="skill_per">90%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Corel
+             </div>
+             <div class="skill_progress">
+               <span style="width: 90%;"></span>
+             </div>
+             <div class="skill_per">90%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               PHP
+             </div>
+             <div class="skill_progress">
+               <span style="width: 50%;"></span>
+             </div>
+             <div class="skill_per">50%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+          <li>
+            <li>      
+            <div class="skill_name">
+                    Indonesia
+                  </div>
+                  <div class="skill_progress">
+                    <span style="width: 90%;"></span>
+                  </div>
+                  <div class="skill_per">90%</div>
+                </li>
+              <li>
+                <div class="skill_name">
+                  English
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 65%;"></span>
+                </div>
+                <div class="skill_per">65%</div>
+              </li>
+              <li>
+                <div class="skill_name">
+                  Aceh
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 90%;"></span>
+                </div>
+                <div class="skill_per">90%</div>
+              </li>
+          </li>
+        </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About us</p>
+         </div>
+        <p>My Name is Aufa Dahlan, I'm 20 Years Old, I From Aceh Besar, Lalhom Kec. Lhoknga, Indonesia</p>
+        <p>I Graduated from State Islamic University of Ar-raniry From Major information Technology</p>
+        <p>My Hoby is Playing Games, Watching movie, and playing Volleyball</p>
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Education</p>
+         </div>
+        <ul>
+            <li>
+              <div class="date">2017 - 2020</div>
+              <div class="info">
+                     <p class="semi-bold">SMKN 1 Banda Aceh</p> 
+                  <p>Major: Desain Grafika</p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2020 - 2024</div>
+              <div class="info">
+                     <p class="semi-bold">State Islamic University</p> 
+                  <p>Major: information Technology</p>
+                </div>
+            </li>
+            
+          </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Work Experience</p>
+         </div>
+      <ul>
+            <li>
+                <div class="date">2020 - 2024</div> 
+                <div class="info">
+                     <p class="semi-bold">still as a student</p> 
+                  
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class="fas fa-bed"></i></li>
+         <li><i class="fas fa-gamepad"></i></li>
+         <li><i class="fas fa-music"></i></li>
+         <li><i class="fas fa-basketball-ball"></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
